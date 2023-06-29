0ac4fd5475f909012d6c4d434966e3af7adb5fb5
Chris Comings <ccomings@gmail.com>
2023-06-29 10:35:15 -0700

    sql connector not always working for flask container

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import os
-from sqlalchemy import create_engine, MetaData, select
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 engine = create_engine("postgresql://postgres:postgres@db:5432/postgres")
 metadata = MetaData()
@@ -19,7 +22,7 @@ session = Session(engine)
 @app.route("/")
 def index():
 	todos = session.query(Todo).all()
-	response = ["----" + todo.text + '\n' for todo in todos]
+	response = ["-" + todo.text + "<br />" for todo in todos]
 	return " ".join(response)
 
 if __name__ == '__main__':
